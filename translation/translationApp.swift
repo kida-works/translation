988d23d3661ed26ae5753807060590e537e8ed36
kida-works <kida.works.1234@gmail.com>
2022-05-28 23:41:22 +0900

    Initial Commit

---
+++ translation/translationApp.swift
@@ -0,0 +1,17 @@
+//
+//  translationApp.swift
+//  translation
+//
+//  Created by KidaTakuya on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct translationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
